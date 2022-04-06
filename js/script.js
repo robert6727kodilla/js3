4512df167b24fc478bf22e386f36d344ce5cdf6c
Robert Lodozynski <robert6727@gmail.com>
2022-04-06 16:04:44 +0200

    poprawki w JS i HTML

--- js/script.js
+++ js/script.js
@@ -1,96 +1,50 @@
-function getMoveName(argMoveId){
-    if(argMoveId == 1){
-      return 'kamień';
+function playGame(playerInput){
+    clearMessages()
+    function getMoveName(argMoveId){
+        if(argMoveId == 1){
+        return 'kamień';
+        }
+        if(argMoveId == 2){
+        return 'papier';
+        }
+        if(argMoveId == 3){
+        return 'nożyce';
+        }
     }
-    if(argMoveId == 2){
-      return 'papier';
+
+    function displayResult(argComputerMove, argPlayerMove){
+        printMessage("komp: " + argComputerMove);
+        printMessage("gracz: " + argPlayerMove);
+        //kamień 
+
+        if(
+            (argComputerMove == 'kamień' && argPlayerMove == 'papier') ||
+            (argComputerMove == 'papier' && argPlayerMove == 'nożyce') || 
+            (argComputerMove == 'nożyce' && argPlayerMove == 'kamień')
+        ) {
+            printMessage('Ty wygrywasz!');
+        } else if(argComputerMove == argPlayerMove) {
+            printMessage('remis!');
+        } else {
+            printMessage('Przegrywasz!');
+        }
     }
-    if(argMoveId == 3){
-      return 'nożyce';
-    }
-  
-    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-    return 'nieznany ruch';
-}
 
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    let computerMove = getMoveName(randomNumber);
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+    //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+    let playerMove = getMoveName(playerInput);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+    displayResult(computerMove, playerMove);
 
-
-let computerMove = getMoveName(randomNumber);
-/*
-if(randomNumber == 1){
-    computerMove = 'kamień';
 }
-    else if(randomNumber == 2){
-    computerMove = 'papier';
-}
-    else if(randomNumber == 3){
-    computerMove = 'nożyce';
-}
-*/
-//printMessage('Mój ruch to: ' + computerMove);
-
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = getMoveName(playerInput);
-/*
-if(playerInput == '1'){
-    playerMove = 'kamień';
-}
-    else if(playerInput == '2'){
-        playerMove = 'papier';
-}
-    else if(playerInput == '3'){
-        playerMove = 'nożyce';
-}
-*/
-//printMessage('Twój ruch to: ' + playerMove);
-
-
-//kamień 
-if( computerMove == 'kamień' && playerMove == 'kamień'){
-    printMessage('remis!');
-}
-    else if( computerMove == 'kamień' && playerMove == 'papier'){
-        printMessage('Ty wygrywasz!');
-}
-    else if( computerMove == 'kamień' && playerMove == 'nożyce'){
-        printMessage('przegrywasz!');
-}
-//papier
-    else if( computerMove == 'papier' && playerMove == 'papier'){
-        printMessage('remis!');
-}
-    else if( computerMove == 'papier' && playerMove == 'nożyce'){
-        printMessage('Ty wygrywasz!');
-}
-    else if( computerMove == 'papier' && playerMove == 'kamień'){
-        printMessage('przegrywasz!');
-}
-
-//nożyce
-    else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
-        printMessage('remis!');
-}
-    else if( computerMove == 'nożyce' && playerMove == 'papier'){
-        printMessage('przegrywasz!');
-}
-    else if( computerMove == 'nożyce' && playerMove == 'kamień'){
-        printMessage('Ty wygrywasz!');
-}
-//inne przypadki
-    else {
-        printMessage('przypadek nie osługiwany');
-}
-
-function displayResult(argComputerMove, argPlayerMove){
-	printMessage("komp: " + argComputerMove);
-    printMessage("gracz: " + argPlayerMove);
-}
-displayResult(computerMove, playerMove);
+document.getElementById('play-rock').addEventListener('click', function(){
+    printMessage(playGame(1));
+});
+document.getElementById('play-paper').addEventListener('click', function(){
+    printMessage(playGame(2));
+});
+document.getElementById('play-scissors').addEventListener('click', function(){
+    printMessage(playGame(3));
+});
